acd2f00d70246a602ce996316b49ef8bc56d261d
Serhii Chebonenko <olenoid@gmail.com>
2020-07-26 20:44:43 +0300

    fist commit

---
+++ coctails.py
@@ -0,0 +1,60 @@
+import requests
+
+def drink_maker(ingredients, image_url):
+    return{
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": ingredients#"*Margarita*\nIngredients:\nTequila - 1 1/2 oz\nTriple sec - 1/2 oz\nLime juice - 1 oz\nSalt"
+        },
+        "accessory": {
+            "type": "image",
+            "alt_text": "drink",
+            "image_url": image_url#"https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg"
+        }
+    }
+
+def howto_maker(howto):
+    return{
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": howto#"*How to:*\nRub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.\nGlass: Cocktail glass"
+        }
+    }
+
+divider_json = {"type": "divider"}
+
+def response_data_combiner(data):
+    drinks = []
+    for drink in data:
+        image_url = drink['strDrinkThumb']
+        ingredients = "*{}*\nIngredients:\n".format(drink['strDrink'])
+        i = 1
+        while(True):
+            ingredients += drink['strIngredient%i' % i]
+            if drink['strMeasure%i' % i] != None:
+                ingredients += ' - ' + drink['strMeasure%i' % i]
+            ingredients += '\n'
+            i += 1
+            if i > 15 :
+                break
+            if drink['strIngredient%i' % i] == None:
+                break
+        howto = "*How to*:\n" + drink['strInstructions']
+        drinks.append(drink_maker(ingredients, image_url))
+        drinks.append(howto_maker(howto))
+        drinks.append(divider_json)
+    return drinks
+
+def get_named_cocktail(name):
+    response = requests.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=%s' % name)
+    data = response.json()
+    return response_data_combiner(data['drinks'])
+    
+    
+def get_random_drink():
+    response = requests.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    data = response.json()
+    return response_data_combiner(data['drinks'])
+#print(get_named_cocktail("margarita"))
---
+++ consolidated.py
@@ -0,0 +1,125 @@
+from flask import Flask, request, make_response, Response
+import os
+import json
+
+from slack import WebClient
+from slackeventsapi import SlackEventAdapter
+
+# Your app's Slack bot user token
+SLACK_BOT_TOKEN = "xoxb-1255022124497-1262254771092-h1QhPerdXBKDgWLdzhvwSQHh"
+SLACK_VERIFICATION_TOKEN = "4v91KH2NgfG9QAXcP62wUmzN"
+SLACK_SIGNING_SECRET = "0af6322b563ff0e37ab0b6f41975cae2"
+
+slack_client = WebClient(SLACK_BOT_TOKEN)
+app = Flask(__name__)
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", app)
+
+import coctails
+
+@slack_events_adapter.on("app_mention")
+def reaction_added(event_data):
+    event = event_data["event"]
+    print(event)
+    channel = event["channel"]
+    text_data = event['text'].split()
+    print(text_data)
+    if text_data[1] == "how" and text_data[2] == "to":
+        drink_name = ' '.join(map(str, text_data[3:]))
+        drink_name = drink_name.replace('*', '')
+        slack_client.chat_postMessage(channel=channel, blocks=coctails.get_named_cocktail(drink_name), text=drink_name)   
+    if text_data[1] == "random" and text_data[2] == "drink":
+        slack_client.chat_postMessage(channel=channel, blocks=coctails.get_random_drink(), text="random")   
+
+
+# Helper for verifying that requests came from Slack
+def verify_slack_token(request_token):
+    if SLACK_VERIFICATION_TOKEN != request_token:
+        print("Error: invalid verification token!")
+        print("Received {} but was expecting {}".format(request_token, SLACK_VERIFICATION_TOKEN))
+        return make_response("Request contains invalid Slack verification token", 403)
+
+
+@app.route("/slack/message_options", methods=["POST"])
+def message_options():
+    # Parse the request payload
+    form_json = json.loads(request.form["payload"])
+
+    # Verify that the request came from Slack
+    verify_slack_token(form_json["token"])
+
+    # Dictionary of menu options which will be sent as JSON
+    menu_options = {
+        "options": [
+            {
+                "text": "Cappuccino",
+                "value": "cappuccino"
+            },
+            {
+                "text": "Latte",
+                "value": "latte"
+            }
+        ]
+    }
+
+    # Load options dict as JSON and respond to Slack
+    return Response(json.dumps(menu_options), mimetype='application/json')
+
+
+# The endpoint Slack will send the user's menu selection to
+@app.route("/slack/message_actions", methods=["POST"])
+def message_actions():
+
+    # Parse the request payload
+    form_json = json.loads(request.form["payload"])
+
+    # Verify that the request came from Slack
+    verify_slack_token(form_json["token"])
+
+    # Check to see what the user's selection was and update the message accordingly
+    selection = form_json["actions"][0]["selected_options"][0]["value"]
+
+    if selection == "cappuccino":
+        message_text = "cappuccino"
+    else:
+        message_text = "latte"
+
+    response = slack_client.chat_update(
+      channel=form_json["channel"]["id"],
+      ts=form_json["message_ts"],
+      text="One {}, right coming up! :coffee:".format(message_text),
+      attachments=[] # empty `attachments` to clear the existing massage attachments
+    )
+
+    # Send an HTTP 200 response with empty body so Slack knows we're done here
+    return make_response("", 200)
+
+# Send a Slack message on load. This needs to be _before_ the Flask server is started
+
+# A Dictionary of message attachment options
+attachments_json = [
+    {
+        "fallback": "Upgrade your Slack client to use messages like these.",
+        "color": "#3AA3E3",
+        "attachment_type": "default",
+        "callback_id": "menu_options_2319",
+        "actions": [
+            {
+                "name": "bev_list",
+                "text": "Pick a beverage...",
+                "type": "select",
+                "data_source": "external"
+            }
+        ]
+    }
+]
+
+# Send a message with the above attachment, asking the user if they want coffee
+#slack_client.chat_postMessage(
+#  channel="#general",
+#  text="Would you like some coffee? :coffee:",
+#  attachments=attachments_json
+#)
+
+# Start the Flask server
+if __name__ == "__main__":
+    app.run()
